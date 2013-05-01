26037b9c2b0df09b5a9761ba360c2f172bef4f31
Fab Mackojc <fab.mackojc@gmail.com>
2013-05-01 10:46:16 -0700

    create skeleton controller and corresponding erbs

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-hacker_news
+# hacker_news
 ===========
+
+
+## Wireframes
+
+- index page (all posts) (/)
+- post page (w/ comments) (/post/id)
+- user profile (/user/id)
+- user page / posts (/user/id/posts)
+- user page / comments (/user/id/comments)
+- register / login (/login)
--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,58 +1,32 @@
-get '/resources' do
-  # Typically the index of a resource returns a collection of the resources
-  # For instances, a blog may have a '/' route that returns all the articles
-  # @resources = Resource.all
-  # ...
+get '/' do 
+  @all_posts = Post.all
+  erb :index  
 end
 
-get '/resources/:id' do
-  # This route would retrieve the specific resource that corresponds with the
-  # params[:id]
-  # @resource = Resource.find(params[:id])
-  # ...
+get '/post/create' do
+  erb :post_create
 end
 
-get '/resources/new' do
-  # This is a get request to retrieve the form that can be used to generate a
-  # new resource
-  # @resource = Resource.new
-  # ...
+get '/post/:id' do
+  @the_post = Post.find(params[:id])
+  erb :post
 end
 
-post '/resources' do
-  # A post to the index to create a new resouce, think of it as adding a new
-  # resource to the collection
-  # Be sure to handle scenarios where the resources is not valid
-  #
-  # @resource = Resource.new(name: 'Kevin')
-  # if @resource.save
-  #   redirect '/'
-  # else
-  #   erb :new
-  # ...cont...
+get '/user/:id/posts' do
+  @the_user = User.find(params[:id])
+  erb :user_posts
 end
 
-get 'resources/:id/edit' do
-  # This is a get request to obtain the resource and the form to make edits
-  # @resource = Resource.find(params[:id])
-  # ...
+get '/user/:id/comments' do
+  @the_user = User.find(params[:id])
+  erb :user_comments
 end
 
-put 'resources/:id' do
-  # This will updated the resource, much like a post you need to make sure
-  # to handle situations that may cause the record to be invalid and fail
-  # to save to the database
-  # @resource = Resource.find(params[:id])
-  # if @resource.update_attributes(params[:resource])
-  #   redirect "/#{@resource.id}"
-  # else
-  #   erb :edit
-  # ...cont...
+get '/user/:id' do
+  @the_user = User.find(params[:id])
+  erb :user
 end
 
-delete 'resources/:id' do
-  # This route will remove the resource from the server
-  # @resource = Resource.find(params[:id])
-  # @resource.destroy
-  # ...
+get '/login' do
+  erb :login
 end
