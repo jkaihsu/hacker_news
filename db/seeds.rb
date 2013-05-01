53c8e0f557e3769d0858b148c40c22cea0521d78
Chris Torstenson <itunes.torstenson@gmail.com>
2013-05-01 14:56:18 -0700

    fix seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@ require 'faker'
 
 number_of_users = 13
 number_of_posts = number_of_users * 7
-number_of_comments = number_of_posts * 13
+number_of_comments = number_of_posts * 30
 
 number_of_users.times do
   User.create(first_name: Faker::Name.first_name,
@@ -15,16 +15,13 @@ users = User.all
 number_of_posts.times do 
   users.sample.posts << Post.create(title: Faker::Lorem.sentence,
                                     link: Faker::Internet.url,
-                                    body: Faker::Lorem.paragraph)
+                                    body: Faker::Lorem.paragraphs)
 end
 posts = Post.all
 
 number_of_comments.times do 
-  the_comment = Comment.new(body: Faker::Lorem.paragraphs)
+  the_comment = Comment.new(body: Faker::Lorem.paragraph)
   the_comment.author = users.sample
   the_comment.post = posts.sample
   the_comment.save
 end
-
-author: users.sample,
-post: posts.sample
